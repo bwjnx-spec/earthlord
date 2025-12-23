83829bb17781f1888b66c57a592e982a085d6a95
bwjtest <bwjnx@qq.com>
2025-12-23 21:09:58 +0800

    Initial Commit

---
+++ earthlord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  earthlord
+//
+//  Created by 何小宝 on 2025/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
